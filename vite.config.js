a0fd05f486444e9a445ff55cc9491a9607dc6854
Joshua Obare <jobare.obare@gmail.com>
2024-05-30 11:55:00 +0300

    intialize project

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: 'resources/js/Dashboard.tsx',
             refresh: true,
         }),
         react(),
