c7a03bf72ee7156d638099b18a5b425b77e766fe
Joshua Obare <jobare.obare@gmail.com>
2024-05-30 15:54:03 +0300

    Update Post and Comment migrations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Comment extends Model
 {
     use HasFactory;
+    protected $table = "comments";
     protected $fillable = [
         "title",
         "postId",
     ];
+    public function user()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
