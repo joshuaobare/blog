772a96d7de01334645341100787e08debcb052ee
Joshua Obare <jobare.obare@gmail.com>
2024-06-03 12:34:53 +0300

    Add FullPost Page and corresponding route in web.php

---
+++ resources/js/Pages/FullPost.tsx
@@ -0,0 +1,61 @@
+import Post from "@/Interfaces/PostInterface";
+import { PageProps } from "@/types";
+
+const FullPost = ({ postData }: PageProps<{ postData: Post }>) => {
+    return (
+        <div className="full-post">
+            <h1>{postData.title}</h1>
+            <div className="full-post-byline">
+                <div>
+                    <b>{postData.author_name}</b>{" "}
+                    <span className="full-post-author-span">Contributor</span>{" "}
+                </div>
+                <div className="full-post-author-span">
+                    {new Date(postData.created_at).toLocaleDateString("en-us", {
+                        weekday: "long",
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                    })}
+                </div>
+            </div>
+            <p className="full-post-text">{postData.body}</p>
+            <h3>Comments</h3>
+            <div className="full-post-comments">
+                {postComments.length !== 0 ? (
+                    postComments.map((comment) => (
+                        <Comment key={comment._id} comment={comment} />
+                    ))
+                ) : (
+                    <div>No Comments</div>
+                )}
+            </div>
+
+            <form
+                onSubmit={handleSubmit}
+                className="full-post-form"
+                method="post"
+            >
+                <div className="form-group">
+                    <label htmlFor="text">
+                        <b>Add Comment</b>
+                    </label>
+                    <textarea
+                        onChange={handleChange}
+                        value={newComment.text}
+                        type="text"
+                        className="form-control text"
+                        name="text"
+                        id="text"
+                    ></textarea>
+                    <input type="hidden" name="postId" value={id} />
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default FullPost;
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/post/{id}', function () {
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
