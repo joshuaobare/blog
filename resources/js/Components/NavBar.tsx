0f6efbc413a7ffd1a6b4ba18329e495f04fc2507
Joshua Obare <jobare.obare@gmail.com>
2024-06-03 10:01:45 +0300

    Update Navbar component and props interface

--- resources/js/Components/NavBar.tsx
+++ resources/js/Components/NavBar.tsx
@@ -1,3 +1,42 @@
-export default function NavBar({ auth }) {
-    return <nav></nav>;
+import { Link } from "@inertiajs/react";
+import "../../css/navbar.css";
+import { FC } from "react";
+
+interface NavBarProps {
+    auth: any;
 }
+
+const NavBar: FC<NavBarProps> = ({ auth }) => {
+    return (
+        <nav className="navbar">
+            <div>The Voice</div>
+            <div className="navbar-links">
+                {auth.user ? (
+                    <Link
+                        href={route("dashboard")}
+                        className="navbar-links rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Dashboard
+                    </Link>
+                ) : (
+                    <>
+                        <Link
+                            href={route("login")}
+                            className="navbar-links rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            href={route("register")}
+                            className="navbar-links rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
