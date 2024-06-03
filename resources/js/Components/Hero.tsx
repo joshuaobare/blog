debba7de2f7715f0f883d8f7ef89e6866d8affde
Joshua Obare <jobare.obare@gmail.com>
2024-06-03 09:48:16 +0300

    Add NavBar component

---
+++ resources/js/Components/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <div>
+            <h1 className="mb-3">The Voice</h1>
+            <h5 className="mb-3">
+                <i>Amplify your voice</i>
+            </h5>
+        </div>
+    );
+}
---
+++ resources/js/Components/NavBar.tsx
@@ -0,0 +1,3 @@
+export default function NavBar({ auth }) {
+    return <nav></nav>;
+}
